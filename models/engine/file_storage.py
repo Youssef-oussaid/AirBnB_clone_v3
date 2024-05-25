cdd6b8273ef92f1ac56200e8b6bf0856f965af1d
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-05-25 14:46:21 +0100

    new commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Retrieves one object"""
+        key = cls.__name__ + '.' + id
+        return self.__objects.get(key, None)
+
+
+    def count(self, cls=None):
+        """Counts the number of objects in storage"""
+        if cls is None:
+            return len(self.__objects)
+        elif cls in classes.values():
+            count = sum(1 for obj in self.__objects.values() if isinstance(obj, cls))
+            return count
+        else:
+            return 0
