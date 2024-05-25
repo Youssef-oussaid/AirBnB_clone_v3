d569849cbc6ddd1ebf01c5d341e5df896ad20552
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-05-26 00:01:16 +0100

    new commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """returns the status in a json form"""
 
-from flask import Flask, jsonify
+from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
+    """return api status"""
     return jsonify({"status": "OK"})
